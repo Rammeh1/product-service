760d9668479e329f31153929f91844c6e697c674
“rammeh” <nasr.rammeh@gmail.com>
2023-11-27 11:54:14 +0100

    first commit

---
+++ src/main/java/org/ms/produitservice/web/ProduitRestService.java
@@ -0,0 +1,27 @@
+package org.ms.produitservice.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Hashtable;
+import java.util.Map;
+
+@RestController
+@RefreshScope
+public class ProduitRestService {
+	 @Value("${monParam}")
+	 private int monParam;
+	 @Value("${email}")
+	 private String email;
+
+	 @GetMapping("config")
+	 public Map<String,Object> config () {
+	 Map<String,Object> params = new Hashtable<>();
+	 params.put("monParam", monParam);
+	 params.put("email", email);
+	 params.put("threadName", Thread.currentThread().toString());
+	 return params;
+	 }
+
+}
